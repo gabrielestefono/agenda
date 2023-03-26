4d1f8fc68d4da30949d6cc91afca510b905ff5fa
gabrielestefono <gabrielestefono@hotmail.com>
2023-03-26 07:11:55 -0300

    Editando Contatos do Banco de Dados

--- config/process.php
+++ config/process.php
@@ -41,10 +41,38 @@ if(!empty($data)){
             $error = $e->getMessage();
             echo "erro: $error";
         }
+    }else if($data["type"] === "edit"){
+
+        $name = $data["name"];
+
+        $phone = $data["phone"];
+
+        $observations = $data["observations"];
+
+        $id = $data["id"];
+
+        $query = "UPDATE contacts SET name=:name, phone=:phone, observations=:observations where id=:id";
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":name",$name);
+
+        $stmt->bindParam(":phone",$phone);
+
+        $stmt->bindParam(":observations",$observations);
+
+        $stmt->bindParam(":id",$id);
+
+        try{
+            $stmt->execute();
+            $_SESSION["msg"] = "Contato Atualizado com sucesso!";
+        }catch(PDOException $e){
+            $error = $e->getMessage();
+            echo "Erro: $error";
+        }
     }
     //redirect HOME
     header("location: " . $BASE_URL . "../index.php");
-
 }else{
 
     //SELEÇÃO DE DADOS
--- templates/header.php
+++ templates/header.php
@@ -29,7 +29,7 @@
             <div>
                 <div class="navbar-nav">
                     <a href="<?=$BASE_URL?>index.php" class="nav-link active" id="homelink">Agenda</a>
-                    <a href="<?=$BASE_URL?>create.php" class="nav-link active" id="homelink">Adicionar Contato</a>
+                    <a href="<?=$BASE_URL?>create.php" class="nav-link active">Adicionar Contato</a>
                 </div>
             </div>
         </nav>
